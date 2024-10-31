8fba5fc871428b0a7977d5a346b4880cf58c0752
dmitryBorisovich <lyubomirov.db@yandex.ru>
2024-11-01 02:24:35 +0300

    Реализован показ ленты с динамической настройкой размеров ячеек, UI-элементы доработаны в соответствии с макетом проекта

--- ImageFeed/ImageList/ImagesListCell.swift
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -1,5 +1,39 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+    
     static let reuseIdentifier = "ImagesListCell"
+    
+    private var gradientLayer: CAGradientLayer?
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var cellLikeButton: UIButton!
+    @IBOutlet weak var cellDateLabel: UILabel!
+        
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configureGradient()
+    }
+    
+    private func configureGradient() {
+        gradientLayer?.removeFromSuperlayer()
+            
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = CGRect(
+            x: 0,
+            y: cellImageView.bounds.height - 30,
+            width: cellImageView.bounds.width,
+            height: 30
+        )
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.ypBlack.withAlphaComponent(0.2).cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1.0)
+            
+        cellImageView.layer.addSublayer(gradientLayer)
+            
+        self.gradientLayer = gradientLayer
+    }
 }
--- ImageFeed/ImageList/ImagesListViewController.swift
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -2,20 +2,41 @@ import UIKit
 
 final class ImagesListViewController: UIViewController {
 
+    // MARK: - Private properties
+    
+    private let photosName: [String] = Array(0..<20).map { "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
     // MARK: - IB Outlets
     
     @IBOutlet private var tableView: UITableView!
     
-    // MARK: - Navigation
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
     
-    func configCell(for cell: ImagesListCell) {
+    // MARK: - Methods
+    
+    func configCell(for cell: ImagesListCell, with index: IndexPath) {
         
+        guard let cellImage = UIImage(named: "\(index.row)") else { return }
+        
+        cell.cellImageView.image = cellImage
+        cell.cellDateLabel.text = dateFormatter.string(from: Date())
+        
+        let isLiked = index.row % 2 == 0
+        let likeImage = isLiked ? UIImage(named: "LikeActive") : UIImage(named: "LikeNoActive")
+        cell.cellLikeButton.setImage(likeImage, for: .normal)
     }
 }
 
@@ -24,7 +45,7 @@ final class ImagesListViewController: UIViewController {
 extension ImagesListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -35,7 +56,7 @@ extension ImagesListViewController: UITableViewDataSource {
             return UITableViewCell()
         }
         
-        configCell(for: imagesListCell)
+        configCell(for: imagesListCell, with: indexPath)
         
         return imagesListCell
     }
@@ -43,8 +64,18 @@ extension ImagesListViewController: UITableViewDataSource {
 
 extension ImagesListViewController: UITableViewDelegate {
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
+        guard let image = UIImage(named: photosName[indexPath.row]) else { return 0 }
+        
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let scaleFactor = imageViewWidth / image.size.width
+        
+        return image.size.height * scaleFactor + imageInsets.top + imageInsets.bottom
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
 }
 
