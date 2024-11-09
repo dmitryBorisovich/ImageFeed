ee03c0fb28e354b06d0a5f182d4be5318dae87cc
dmitryBorisovich <lyubomirov.db@yandex.ru>
2024-11-09 17:26:11 +0300

    Экран профиля переделан под верстку кодом

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -49,7 +49,7 @@ final class SingleImageViewController: UIViewController {
         
         let hScale = visibleRectSize.width / imageSize.width
         let vScale = visibleRectSize.height / imageSize.height
-        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
         
         scrollView.setZoomScale(scale, animated: false)
         scrollView.layoutIfNeeded()
--- ImageFeed/UserProfile/ProfileViewController.swift
+++ ImageFeed/UserProfile/ProfileViewController.swift
@@ -2,22 +2,104 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    // MARK: - IB Outlets
-    
-    @IBOutlet private weak var userNameLabel: UILabel!
-    @IBOutlet private weak var userIdLabel: UILabel!
-    @IBOutlet private weak var userImageView: UIImageView!
-    @IBOutlet private weak var userTextLabel: UILabel!
+    // MARK: - Private properties
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .ypBackground
+        
+        let userImageView = addUserImageView()
+        let userTextLabels = addUserTextLabels()
+        let logOutButton = addLogOutButton()
+        
+        setupConstraints(image: userImageView, labels: userTextLabels, button: logOutButton)
     }
     
-    // MARK: - IB Actions
+    // MARK: - Private methods
     
-    @IBAction func logOutButtonPressed() {
+    private func addUserImageView() -> UIImageView {
+        let userImageView = UIImageView()
+        userImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userImageView)
+        
+        userImageView.tintColor = .gray
+        userImageView.layer.cornerRadius = 35
+        userImageView.image = UIImage(named: "UserMockImage") ?? UIImage(systemName: "person.crop.circle.fill")
+        
+        return userImageView
+    }
+    
+    private func addUserTextLabels() -> UIStackView {
+        let userNameLabel = UILabel()
+        userNameLabel.text = "Екатерина Новикова"
+        userNameLabel.font = .systemFont(ofSize: 23, weight: .bold)
+        userNameLabel.textColor = .ypWhite
+        
+        let userIdLabel = UILabel()
+        userIdLabel.text = "@ekaterina_nov"
+        userIdLabel.font = .systemFont(ofSize: 13)
+        userIdLabel.textColor = .ypGray
+        
+        let userDescriptionLabel = UILabel()
+        userDescriptionLabel.text = "Hello, world!"
+        userDescriptionLabel.font = .systemFont(ofSize: 13)
+        userDescriptionLabel.textColor = .ypWhite
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            userNameLabel,
+            userIdLabel,
+            userDescriptionLabel
+        ])
+        
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        
+        return stackView
+    }
+    
+    private func addLogOutButton() -> UIButton {
+        let logOutButton = UIButton.systemButton(
+            with: UIImage(named: "LogOut") ?? UIImage(),
+            target: self,
+            action: #selector(logOutButtonPressed)
+        )
+        logOutButton.tintColor = .ypRed
+        
+        logOutButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logOutButton)
+        
+        return logOutButton
+    }
+    
+    private func setupConstraints(image: UIImageView, labels: UIStackView, button: UIButton) {
+        NSLayoutConstraint.activate([
+            image.widthAnchor.constraint(equalToConstant: 70),
+            image.heightAnchor.constraint(equalToConstant: 70),
+            image.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            image.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            button.widthAnchor.constraint(equalToConstant: 44),
+            button.heightAnchor.constraint(equalToConstant: 44),
+            button.centerYAnchor.constraint(equalTo: image.centerYAnchor),
+            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -14)
+        ])
+        
+        NSLayoutConstraint.activate([
+            labels.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 110),
+            labels.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            labels.trailingAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 8)
+        ])
+    }
+    
+    @objc private func logOutButtonPressed() {
         
     }
 }
